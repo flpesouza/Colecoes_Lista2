4fa1ddd8b0052ef1dfaf65c74a0fa7c6b70683ae
Felipe Souza <67338813+flpesouza@users.noreply.github.com>
2023-10-03 15:48:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<double> polonesa = new Stack<double>();
+            
+            double resultado;
+
+            Console.WriteLine("Digite a expressão polonesa: ");
+            string expressao = Console.ReadLine();
+
+            char[] charArray = expressao.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (charArray[i] != '+' && charArray[i] != '-' && charArray[i] != '*' && charArray[i] != '/')
+                {
+                    char letra = charArray[i];
+                    double num = (int)Char.GetNumericValue(letra);
+                    polonesa.Push(num);
+                }
+                else if (charArray[i] == '+')
+                {
+                    double num1 = polonesa.Pop();
+                    double num2 = polonesa.Pop();
+                    resultado = num2 + num1;
+                    polonesa.Push(resultado);
+                }
+                else if (charArray[i] == '-')
+                {
+                    double num1 = polonesa.Pop();
+                    double num2 = polonesa.Pop();
+                    resultado = num2 - num1;
+                    polonesa.Push(resultado);
+
+                }
+                else if (charArray[i] == '*')
+                {
+                    double num1 = polonesa.Pop();
+                    double num2 = polonesa.Pop();
+                    resultado = num2 * num1;
+                    polonesa.Push(resultado);
+
+                }
+                else if (charArray[i] == '/')
+                {
+                    double num1 = polonesa.Pop();
+                    double num2 = polonesa.Pop();
+                    resultado = num2 / num1;
+                    polonesa.Push(resultado);
+
+                }
+            }
+            resultado = polonesa.Pop();
+            Console.WriteLine("Resultado: " + resultado);
+            Console.ReadLine();
+        }
+    }
+}
